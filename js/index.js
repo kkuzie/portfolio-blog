850dd7241ff009697fa3042b406ea10750b8ae80
Karen Kuzi <kkuzii@yahoo.it>
2021-06-15 12:53:42 -0700

    added animations to index.html

--- js/index.js
+++ js/index.js
@@ -1,29 +1,60 @@
-
+gsap.registerPlugin(ScrollTrigger);
+// gsap.registerPlugin(ScrollToPlugin);
 //adds or removes .nav-open which adds position fixed to hamburger
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link');
+// const addZindex = document.querySelector('.intro');
+// console.log(addZindex);
 
 navToggle.addEventListener('click', () => {
+    //*brings sections to the top of .nav so hovering works, then js brings it back to 0 when nav menu appears*
     document.body.classList.toggle('nav-open');
+    if (document.body.classList.contains('nav-open')){
+    $('.portfolio').css('z-index', '0');
+    $('footer').css('z-index', '0');
+    $('section').css('z-index', '0');    
+    } else {
+        //*brings section to the top of .nav so hovering works, then js brings it back to 0 when nav menu appears*
+        window.setTimeout(function(){
+            $('.portfolio').css('z-index', '100');
+            $('section').css('z-index', '100');
+            $('footer').css('z-index', '100');
+        }, 3000);
+    }
+    // document.addZindex.css('z-index', '100');
+    // $('.intro').css('z-index', '0');
 });
 
+///////removes nav-open so flyout disappears 
 navLinks.forEach((link) => {
     link.addEventListener('click', () => {
         document.body.classList.remove('nav-open');
     })
-})
+});
 
 
+//DELETE - doesnt work - go to section
+// const works = document.querySelector('.btn');
+// console.log(works);
+
+// works.onclick = function(e) {
+//     e.preventDefault();
+//     document.getElementById('whatIdo').scrollIntoView({
+//         behavior: 'smooth'
+//     });
+// }
 
 // for animated flyout menu
 $(document).ready(function () {
 
+////////move hover bar on mouse move
     $(window).mousemove(function (e) {
         var y = e.clientY;
         // console.log(y);
         $('.nav__item--hover-element').css('margin-top', y);
     });
 
+////////changes the images on hover of menu links
     $('.nav__link').mouseover(function() {
         var activeLinkId = $(this).attr('href');
         // console.log(activeLinkId);//shows that when hover which links are active
@@ -31,123 +62,146 @@ $(document).ready(function () {
         $('.nav__img--link-img' + activeLinkId).addClass('active');
     });
 
-    var toggleBtn = document.querySelector('.nav-toggle');
+    // var toggleBtn = document.querySelector('.nav-toggle');//using const navToggle instead (above)
 
-    //gsap animation in nav menu
-    var tl = new TimelineMax({paused: true});
+    var tlPeekInR = new TimelineMax({paused: true});
+    var tlPeekInL = new TimelineMax({paused: true});
+    // var tlPeekInBoth = new TimelineMax({paused: true});
 
-    //hamburger animation
-    tl.to('.hamburger', .5, {
-        // right: '-500'
-        rotate: 90
-    })
-    tl.to('.hamburger-top', .5, {
-        rotate: 45,
-        top: 10
-    }, '-=.5')//offset
-    tl.to('.hamburger-bottom', .5, {
-        rotate: -45,
-        top: 10
-    }, '-=.5')//offset
-
-// entire menu slides in:
-    tl.to('.nav', 1, {
-        right: '0'
+////////graphic design side
+    tlPeekInR.from('.intro__img--sideways', .8, {
+        x: 310
     })
 
-    // menu items stagger into view
-    tl.from('nav ul li', .8, {
-        x: -1000,
-        opacity: 0,
-        stagger: .3
-    })
+    tlPeekInR.from('.section__subtitle--gd', 1,{
+        x: 650
+    }, '-=.8')
 
-    //bar comes into view
-    tl.from('.nav__item--hover-element', 1, {
-        x: -1000,
-        opacity: 0
-    }, '-=1' )
-
-    // changes an image when hover over menu item
-    // tl.from('.nav__img', 1.4, {
-    //     x: 1000,
-    // })
-
-    var tlSlideIn = new TimelineMax({paused: true});
-
-    tlSlideIn.from('.nav__img--home', .15, {
-        y: 1500
+    $(document).on('mouseenter', ".intro__gd", function(){
+        tlPeekInR.play();
+    }).on('mouseleave', '.intro__gd', function(){
+        tlPeekInR.reverse();
     });
 
-    tlSlideIn.from('.nav__img--whatIdo', .15,{
-        x: 1000
+////////front end side
+    tlPeekInL.from('.intro__img--silly', .8, {
+        x: -340,
+        rotate: 40,
     })
 
-    tlSlideIn.from('.nav__img--about', .15, {
-        y: -1000
-    })
+    tlPeekInL.from('.section__subtitle--fed', 1, {
+        x: -600,
+    }, '-=.8')
 
-    tlSlideIn.from('.nav__img--work', .2, {
-        x: -1000,
-        rotate: 90
-    })
-
-    $(document).on('mouseenter', ".nav__link", function(){
-        tlSlideIn.play();
-        console.log('mouseentered');
-    }).on('mouseleave', '.nav__link', function(){
-        tlSlideIn.reverse();
-        console.log('mouseleave');
+    $(document).on('mouseenter', ".intro__fed", function(){
+        tlPeekInL.play();
+    }).on('mouseleave', '.intro__fed', function(){
+        tlPeekInL.reverse();
     });
 
-//    $('.nav__link--home').mouseenter(function() {
-//     tl.from('.nav__img--home', 1.4, {
-//         y: 1500
-//     })
-//     console.log('mouseenter slide in');
-// });
-// $('.nav__link--home').mouseleave(function() {
-//     tl.to('.nav__img--home', 1.4, {
-//         y: 0
-//     })
-//     console.log('mouseenter slide out');
-// });
+//////////////////////both graphic design and front end at same time
+// tlPeekInBoth.from('.intro__img--sideways', .8, {
+//     x: 310
+// })
 
-////////////////////////////////////////////////////////////////    
-// $('.nav__link--whatIdo').mouseenter(function() {
-//     tl.from('.nav__img', 1.4, {
-//         x: 1000
-//     })
+// tlPeekInBoth.from('.section__subtitle--gd', 1,{
+//     x: 650
+// }, '-=.8')
 
-// });
-// ////////////////////////////////////////////////////////////////
-// $('.nav__link--about').mouseenter(function() {
-//     tl.from('.nav__img', 1.4, {
-//         y: -500
-//     })
-// });
-// //////////////////////////////////////////////////////////////////
-// $('.nav__link--work').mouseenter(function() {
-//     tl.from('.nav__img', 1.4, {
-//         x: 1000,
-//         rotate: 90
-//     })
-// });
+// tlPeekInBoth.from('.intro__img--silly', .8, {
+//     x: -340,
+//     rotate: 40,
+// }, '-=1.8')
+
+// tlPeekInBoth.from('.section__subtitle--fed', 1, {
+//     x: -600,
+// }, '-=2.6')
 
 
-    
-
-
-
-
-    //timeline reverse
-    tl.reverse();
-
-    //click to play timeline
-    toggleBtn.onclick = function() {
-        tl.reversed(!tl.reversed());
-    }
+////////on hover of h1 - both graphic design and front end dev slides in
+$(document).on('mouseover', ".section__title--intro", function(){
+    tlPeekInR.play();
+    tlPeekInL.play();
+}).on('mouseout', '.section__title--intro', function(){
+    tlPeekInR.reverse();
+    tlPeekInL.reverse();
 });
+
+
+
+
+////////WHAT I DO section animations
+        // tlBlurbs = new TimelineMax();
+        
+        gsap.from('.whatIdo__descriptions--each', {
+            x: -2000,
+            duration: 3,
+            opacity: .5,
+            stagger: -.3,
+            scrollTrigger:  {
+                trigger: ".whatIdo__descriptions",
+                start: 600,
+                toggleActions: 'restart none none none',
+                // markers: true
+                // markers: {
+                //     startColor: 'yellow',
+                //     endColor: 'lime',
+                //     fontSize: '2em',
+                //     indent: 200
+                // }
+            }
+        })
+
+////////about section animations
+        gsap.from('.section__subtitle--about', {
+            x: 1000,
+            duration: 3,
+            scrollTrigger: {
+                trigger: ".section__subtitle--about", 
+                toggleActions: 'restart none none none'
+            }
+        })
+
+        gsap.from('.section__title--about', {
+            x: -500,
+            duration: 1.4,
+                scrollTrigger: {
+                trigger: '.section__subtitle--about',
+                toggleActions: 'restart none none none'
+            }
+        })
+
+        gsap.from('.about-me__body', {
+            opacity: 0,
+            duration: 4,
+            stagger: true,
+            scrollTrigger: {
+                trigger: '.about-me__body',
+                toggleActions: 'restart none none none',
+                // markers: {
+                //     startColor: 'fuschia',
+                //     endColor: 'white'
+                // }
+            }
+        })
+
+////////peek-a-boo image
+        gsap.from('.my-work__img', {
+            y: 300,
+            duration: 1.4,
+            scrollTrigger: {
+                trigger: ".section__subtitle--tagline",
+                toggleActions: 'restart reverse restart reverse',
+                // markers: {
+                //     startColor: 'yellow',
+                //     endColor: 'lime',
+                //     fontSize: '2em',
+                //     indent: 200
+                // }
+            }
+        })
+});//document.ready closing
+    // console.log(window.location.href);
 
 // let navLink = $('.nav__link');
 // let hover =
@@ -158,4 +212,4 @@ $(document).ready(function () {
 
 // navLink.addEventListener('mouseenter', () => hover.play());
 // navLink.addEventListener('mouseleave', () => hover.reverse());
-    
+
