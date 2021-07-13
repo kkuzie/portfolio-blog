76b7d0db80c526d2ca0c7ed592e2b325da82140e
Karen Kuzi <kkuzii@yahoo.it>
2021-07-13 11:41:20 -0700

    created separate js file for header nav scrollup

---
+++ js/headerNavScrollup.js
@@ -0,0 +1,33 @@
+//////// HEADER NAV bar reappear on scroll up
+//////// https://www.w3schools.com/howto/howto_js_navbar_hide_scroll.asp////////
+
+//////// need to deactivate onclick of navlink
+let navAppearOnScrollup = function () {
+
+    var prevScrollpos = window.pageYOffset;
+    window.onscroll = function () {
+        var currentScrollPos = window.pageYOffset;
+        if (prevScrollpos > currentScrollPos) {
+            document.querySelector("header").style.top = "0";
+        } else {
+            document.querySelector("header").style.top = "-180px";
+        }
+        prevScrollpos = currentScrollPos;
+    }
+    }
+    navAppearOnScrollup();
+    
+    
+    let pauseNavAppearOnScrollup = function () {
+    var prevScrollpos2 = window.pageYOffset;
+    window.onscroll = function () {
+        var currentScrollPos2 = window.pageYOffset;
+        if (prevScrollpos2 < currentScrollPos2) {
+            document.querySelector("header").style.top = "-180px";
+        } else {
+            document.querySelector("header").style.top = "-180px";
+        }
+        prevScrollpos2 = currentScrollPos2;
+    }
+    }
+    
