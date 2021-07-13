661f0cc13037cdb5be69a5bc90c93ce9c66cceb4
Karen Kuzi <kkuzii@yahoo.it>
2021-07-13 11:36:52 -0700

    add history back function for portfolio page arrow

---
+++ js/portfolio.js
@@ -0,0 +1,32 @@
+function goBack() {
+    window.history.back();
+  }
+//   function goForward() {
+//     window.history.forward();
+//   }
+
+// var mute=document.getElementsByClassName("unmute") ;   
+// var video=document.getElementsByTagName('video');
+// console.log(mute);
+// console.log(video);
+
+// function toggleMute() {
+//   var video=document.getElementsById('commercialPlay')
+
+//   if(video.muted){
+//     console.log(video.muted);
+//     video.muted = false;
+//     // video.autoplay = false;
+//     console.log('yes sound');
+//   } else {
+//     video.muted = true;
+//     // video.autoplay = true;
+//     console.log('no sound');
+//   }
+// console.log(video.muted);
+// // console.log(video.autoplay);
+// }
+
+// $(mute).on("click", function(e){
+//   toggleMute();
+// });
